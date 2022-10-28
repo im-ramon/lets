f56feea75bc5240b7f4e4fd4b05f176ac9e31324
Ramon Oliveira <ramonoliveirasantos11@gmail.com>
2022-10-27 21:04:06 -0300

    style: :art: Inclusão da logo na tela de carregamento
    Inclusão da logo na tela de carregamento

--- src/components/Views/Loading/index.tsx
+++ src/components/Views/Loading/index.tsx
@@ -1,13 +1,16 @@
-import { View, ActivityIndicator, StyleSheet } from 'react-native'
+import { View, ActivityIndicator, StyleSheet, Image, Text } from 'react-native'
 import { THEME } from '../../../theme/index'
+import logo from '../../../assets/img/icon.png'
 
 export function Loading() {
     return (
         <View style={styles.container}>
-            <ActivityIndicator
-                color={THEME.COLORS.PRIMARY}
-                size={32}
-            />
+            <View style={styles.logoArea}>
+                <Image source={logo} style={styles.logo} />
+                <Text style={styles.text}>
+                    <Text style={styles.strong}>Let's!</Text> <Text style={styles.pipe}>|</Text> Especialista X</Text>
+            </View>
+            <ActivityIndicator color={THEME.COLORS.PRIMARY} size={32} />
         </View>
     )
 }
@@ -18,5 +21,27 @@ export const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: THEME.COLORS.BACKGROUND_1
+    },
+    text: {
+        fontSize: THEME.FONT_SIZE.MD,
+        color: THEME.COLORS.TEXT
+    },
+    strong: {
+        fontWeight: 'bold',
+    },
+    pipe: {
+        color: THEME.COLORS.NEUTRAL_2
+    },
+    logoArea: {
+        padding: 8,
+        marginBottom: 16,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    logo: {
+        width: 32,
+        height: 32,
+        marginRight: 8
     }
 });
